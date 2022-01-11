0d64322abb2385ced1629a8c127e2ea416763eca
Giovani Girardi <giovanigg08@gmail.com>
2022-01-11 20:07:43 -0300

    feat: Configurado useEffect para fazer a requisição à API do github

--- src/components/RepositoryItem/index.jsx
+++ src/components/RepositoryItem/index.jsx
@@ -4,7 +4,7 @@ export const RepositoryItem = ({ repository }) => {
       <strong>{repository.name}</strong>
       <p>{repository.description}</p>
 
-      <a href={repository.link}>Acessar repositório</a>
+      <a href={repository.html_url}>Acessar repositório</a>
     </li>
   )
 }
